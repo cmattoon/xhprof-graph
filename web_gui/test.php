1137c303c8f52778e22680d9eddd0a1eb1beb1f9
Curtis Mattoon <cmattoon@cmatttoon.com>
2015-04-11 23:29:17 -0400

    NewParser + test.php (working)

--- web_gui/test.php
+++ web_gui/test.php
@@ -5,7 +5,17 @@ define('DB_PORT', 7474);
 define('DB_USER', 'neo4j'); // The username for the Neo4j instance
 define('DB_PASS', 'password');
 
-$xhprof_file = '5518a72535298.benchmark_1427679013.xhprof';
+
+NewParser::importAll('/tmp/xhdata/');
+die('DONE HERE<hr>');
+$file = '';
+if (!isset($_GET['f'])) {
+    foreach(glob("/tmp/xhdata/*.xhprof") as $f) {
+	echo "<div><a href='{$_SERVER['REQUEST_URI']}?f={$f}'>{$f}</a></div>\n";
+    }
+} else {
+    $file = basename($_GET['f']);
+}
 
 try {
     $client = new Everyman\Neo4j\Client(DB_HOST, DB_PORT);
@@ -14,9 +24,10 @@ try {
      * Do that here.
      */
     $client->getTransport()->setAuth(DB_USER, DB_PASS);
+    echo "<div>Parsing {$file}</div>\n";
 
     // See dir property in Parser class
-    $parser = new Parser($file);
+    $parser = new NewParser($file);
     
     // Do things
     $parser->makeGraph();
@@ -24,3 +35,4 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage();
 }
+echo "<hr>Done\n";
