737b4c4019f2d525afb6d740e65f3feb3b9b07bf
Curtis Mattoon <cmattoon@cmattoon.com>
2015-04-13 10:44:24 -0400

    squashme maybe?

--- web_gui/inc/config.php
+++ web_gui/inc/config.php
@@ -9,6 +9,7 @@ foreach ($config as $k=>$v) {
     define($k, $v);
 }
 require_once('functions.php');
+
 function get_client() {
     $client = new Everyman\Neo4j\Client(DB_HOST, DB_PORT);
     $client->getTransport()->setAuth(DB_USER, DB_PASS);
