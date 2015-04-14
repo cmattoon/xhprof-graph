c77b4eb088a7bc0ea8def23d4bd771b30a1873a8
Curtis Mattoon <cmattoon@cmattoon.com>
2015-04-14 17:08:38 -0400

    Moved upload to runs.php

--- web_gui/runs.php
+++ web_gui/runs.php
@@ -1,9 +1,31 @@
 <?php
 require_once('inc/config.php');
 $client = get_client();
-$query = "MATCH (n:Callable) WHERE (HAS(n.runId) AND HAS(n.name) AND n.name = 'main()') RETURN n.runId, n.scriptName";
 
-$q = new Everyman\Neo4j\Cypher\Query($client, $query);
+/**
+ * Handle *.xhprof file upload
+ */
+if (isset($_FILES['xhprof_file'])) {
+    $file = $_FILES['xhprof_file'];
+    if ($file['error'] === 0) {
+	$newfile = "/tmp/xhdata/{$file['name']}";
+	if (move_uploaded_file($file['tmp_name'], $newfile)) {
+	    $parser = new NewParser($file['name']);
+	    $parser->makeGraph();
+	    $msgs[] = "Imported {$newfile}";	    
+	} else {
+	    $msgs[] = "Can't import {$file['tmp_name']}";
+	}
+    }
+}
+/**
+ * Get run data from DB
+ */
+$q = new Everyman\Neo4j\Cypher\Query($client, 
+				     "MATCH (n:Callable) 
+WHERE (HAS(n.runId) AND HAS(n.name) AND n.name = 'main()') 
+RETURN n.runId, n.scriptName");
+
 $res = $q->getResultSet();
 
 $runs = array();
@@ -17,4 +39,4 @@ foreach ($res as $r) {
 }
 
 $tpl = new Template();
-$tpl->displayPage(array('runs' => $runs));
+$tpl->displayPage(array('runs' => $runs, 'msgs'=>$msgs));
