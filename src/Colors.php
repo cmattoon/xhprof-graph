89cf636f5e935ae357f3d47395c7829e027b8ea1
Curtis Mattoon <cmattoon@cmatttoon.com>
2015-05-20 21:32:25 -0400

    Colors class (gross)

---
+++ src/Colors.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * For accessing color info.
+ */
+class Colors {
+    /**
+     * Colors for chart.js
+     */
+    public static $colors = array(
+	/* Blue */
+	'rgba(20, 151, 227, 0.95)',
+	'rgba(20, 151, 227, 0.55)',
+	'rgba(20, 151, 227, 0.25)',
+	/* Green */
+	'rgba(20, 227, 151, 0.95)',
+	'rgba(20, 227, 151, 0.55)',
+	'rgba(20, 227, 151, 0.25)',
+	/* Orange */
+	'rgba(227, 151, 20, 0.95)',
+	'rgba(227, 151, 20, 0.55)',
+	'rgba(227, 151, 20, 0.25)',
+	/* Yellow */
+	'rgba(151, 227, 20, 0.95)',
+	'rgba(151, 227, 20, 0.55)',
+	'rgba(151, 227, 20, 0.25)',
+    );
+}
