4bffc655c1642e15aeca30c92ebf4c4b8b404059
Curtis Mattoon <cmattoon@cmatttoon.com>
2015-05-20 21:29:17 -0400

    Chart.js for run-info.php

--- web_gui/run-info.php
+++ web_gui/run-info.php
@@ -8,10 +8,16 @@ $script = '';
 if ($runId) {
     $ql = new QueryList();
     $run = new Run($runId);
-    $runs = $run->getRunStats();
+    $runs = $run->getRunStats(true, array('r.wt'=>'desc','r.cpu'=>'desc'));
     $script = $ql->getScriptNameFromRunId($runId);
+    $jsonWt = $run->getJSONForPieChart('wt');
+    $jsonCpu = $run->getJSONForPieChart('cpu');
+    $jsonMu = $run->getJSONForPieChart('mu');
+    $jsonPmu = $run->getJSONForPieChart('pmu');
 }
 
 $tpl = new Template();
 $tpl->displayPage(array('runs' => $runs, 'runId' => $runId, 
-			'bcrun' => $runId, 'bcscript' => $script));
+			'bcrun' => $runId, 'bcscript' => $script,
+			'jsonWt' => $jsonWt, 'jsonCpu' => $jsonCpu,
+			'jsonMu' => $jsonMu, 'jsonPmu' => $jsonPmu));
